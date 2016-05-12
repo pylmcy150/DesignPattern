ad85f4d2d4c0f382e442aa9be31fea6b0c6a34a8
陈柯梦 <pylmcy150@sina.cn>
2016-05-13 01:13:01 +0800

    更新项目基础结构

---
+++ DesignPattern/DotNet/UnitTestProject/GeneratedCode/SingletonTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class SingletonTests
+    {
+        [TestMethod()]
+        public void GetInstanceTest()
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance() ;
+            Assert.AreEqual<Singleton>(s1, s2);
+        }
+    }
+}
---
+++ DesignPattern/Javascript/JsPattern/Scripts/js/requireconfig.js
@@ -0,0 +1,19 @@
+﻿requirejs.config({
+    baseUrl: "js/",
+    paths: {
+        qunit: "../Scripts/qunit-1.14.0"
+    },
+    shim: {
+        qunit: {
+            exports: "QUnit"
+        }
+    }
+});
+
+require(["qunit"], function (QUnit) {
+    QUnit.load();
+
+    QUnit.test("hello test", function (assert) {
+        assert.ok(1 == "1", "Passed!");
+    })
+});
