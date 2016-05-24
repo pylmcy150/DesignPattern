70e5a2594a20af4bb298b78e4654dee54e001924
陈柯梦 <pylmcy150@sina.cn>
2016-05-24 22:43:49 +0800

    构造模式

---
+++ DesignPattern/DotNet/UnitTestProject/GeneratedCode/BuildTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject.GeneratedCode
+{
+    [TestClass]
+    public class BuildTests
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Director d = new Director();
+            //构建
+            Builder b = new ConcreteBuilder();
+            d.Constructor(b);
+            Product p = b.GetResult();
+            p.Show();
+
+            
+        }
+    }
+}
